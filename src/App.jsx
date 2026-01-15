cc34eec2fe7c9cefcae3627daa92c2c930fae719
Mahmoud Abd Elfatah <mahmdmndbd@gmail.com>
2026-01-15 07:57:17 -0800

    Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Login from "./auth/Login/Login.jsx"
 import Register from "./auth/Register/Register.jsx"
 import Notfound from "./pages/Notfound/Notfound.jsx"
 import { RecoilRoot } from "recoil"
+import ProtectedRoute from "./auth/ProtectedRoute/ProtectedRoute.jsx"
 
 
 
 let routes = createBrowserRouter([
   {
     path: "", element: <Layout />, children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <ProtectedRoute><Home /></ProtectedRoute> },
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
       { path: "*", element: <Notfound /> }
